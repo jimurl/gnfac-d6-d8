727b95e30702ff1ca1a526c282a705c13d998177
jimurl <jimurl@montanaice.com>
2018-06-28 12:09:51 -0600

    clarity update

--- gnfac_d8/js/gnfac-weather.js
+++ gnfac_d8/js/gnfac-weather.js
@@ -3,17 +3,11 @@ jQuery(document).ready(function ($) {
 			var bridgers_station = '_'+$('#edit-field-bridgers-weather-station').val() ;
 			
 			var bridgers_station_values = { swe: '1.03', snow_depth: '44.2' };
-			drupalSettings.gnfac_d8.gnfac_weather.bridgers.forEach( bridgerStations );
 			
-			console.log(drupalSettings.gnfac_d8.gnfac_weather.bridgers.bridgers_station.swe );
 			console.log(drupalSettings.gnfac_d8.gnfac_weather.bridgers._13920.swe );
 			
 			console.log( 'bridgers station: ' + bridgers_station);
-      //$('#edit-field-bridgers-swe-0-value').val(drupalSettings.gnfac_d8.gnfac_weather.bridgers..swe );
+      $('#edit-field-bridgers-swe-0-value').val(drupalSettings.gnfac_d8.gnfac_weather.bridgers._13920.swe );
 
     });
 });
-
-function bridgerStations( item, key, array ){
-	$('#edit-field-bridgers-swe-0-value').val
-}
