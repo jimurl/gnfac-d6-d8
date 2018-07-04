8665c5de221588d4e8c816c6111a65d6324d5b61
jdebr <jd.debr@gmail.com>
2018-07-04 11:28:03 -0600

    Added handler to fill out text fields from drupalSettings when region selector is changed

--- gnfac_d8/js/gnfac-weather.js
+++ gnfac_d8/js/gnfac-weather.js
@@ -1,14 +1,26 @@
 jQuery(document).ready(function ($) {
-	console.log("Joe Debug");
-    $('select[name="field_bridgers_weather_station"]').change(function() {
-			var bridgers_station = '_'+$('#edit-field-bridgers-weather-station').val() ;
-			
-			var bridgers_station_values = { swe: '1.03', snow_depth: '44.2' };
-			
-			console.log(drupalSettings.gnfac_d8.gnfac_weather.bridgers._13920.swe );
-			
-			console.log( 'bridgers station: ' + bridgers_station);
-      $('#edit-field-bridgers-swe-0-value').val(drupalSettings.gnfac_d8.gnfac_weather.bridgers._13920.swe );
+		// Detect change events on the region selects and fill out the associated text fields
+		// with values from the drupalSettings array
+		$('#node-wx-log-form').on('change', 'select[id^=edit-field][id$=weather-station]', function(e) {
+				// the value of the select is the station ID
+				// prepend an underscore to match drupalSettings array
+				var stationId = '_' + e.target.value;
 
-    });
-});
+				// parse out region name from the element ID
+				var re = /edit-field-(.*)-weather-station/;
+				var regionSelector = e.target.id.match(re)[1];
+				// convert dash to underscore for drupalSettings array
+				var regionSetting = regionSelector.replace('-', '_');
+	
+				$('#edit-field-' + regionSelector + '-swe-0-value').val(
+					drupalSettings.gnfac_d8.gnfac_weather[regionSetting][stationId].swe);
+				$('#edit-field-' + regionSelector + '-snow-depth-0-value').val(
+					drupalSettings.gnfac_d8.gnfac_weather[regionSetting][stationId].depth);
+				$('#edit-field-' + regionSelector + '-wind-avg-0-value').val(
+					drupalSettings.gnfac_d8.gnfac_weather[regionSetting][stationId].wind);
+				$('#edit-field-' + regionSelector + '-wind-gust-0-value').val(
+					drupalSettings.gnfac_d8.gnfac_weather[regionSetting][stationId].wind_gust);
+				$('#edit-field-' + regionSelector + '-wind-dir-0-value').val(
+					drupalSettings.gnfac_d8.gnfac_weather[regionSetting][stationId].wind_dir);
+		});
+})(jQuery)
